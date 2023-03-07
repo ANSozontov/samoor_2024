ba30782d05ca5ec6169b63708ef8a4b0ccc83822
ANSozontov <A.N.Sozontov@gmail.com>
2023-03-08 01:21:24 +0500

    ап!

--- counts.viz_06.03.2023.R
+++ counts.viz_06.03.2023.R
@@ -1,5 +1,6 @@
 # loading -----------------------------------------------------------------
 library(tidyverse)
+theme_set(theme_bw() + theme(legend.position = "bottom"))
 library(parallel)
 cl <- makeCluster(detectCores()-1)
 
@@ -25,6 +26,8 @@ labs <- readxl::read_excel("Caspian data_01.03.2023_SA.xlsx", sheet = "samples")
     filter(distr == "Samoor") %>% 
     transmute(id = substr(id, 3, 7), coast, plants.d) %>% 
     separate(plants.d, sep = " ", into = "plants.d", extra = "drop")
+taxa <- readxl::read_excel("Caspian data_01.03.2023_SA.xlsx", sheet = "taxa") %>% 
+    select(sp, area2, order)
 
 # rarefication ------------------------------------------------------------
 # Oribatida - d20
@@ -74,7 +77,7 @@ div <- long %>%
     pivot_wider(names_from = sp, values_from = abu) %>% 
     select(-O, -age) %>% 
     left_join(div, ., by = "id") 
-
+div
 # multivariate ------------------------------------------------------------
 dis <- list()
 # dissimilarity
@@ -122,9 +125,150 @@ PCOA <- dis %>%
     purrr::transpose()
 rm(cl)
 
-# Viz me ---------------------------------------------------------------
+# Viz me 1 ---------------------------------------------------------------
+abundance <- div %>% select(id, coast, plants.d, Collembola:Oribatida_total) %>% 
+    pivot_longer(names_to = "taxa", values_to = "abu", -c(id:plants.d)) %>% 
+    # pivot_longer(names_to = "VAR", values_to = "VAL", -c("id", "taxa", "abu")) %>% 
+    mutate(seria = substr(id, 1, 4), .before = 2, 
+           taxa = str_replace_all(taxa, "_total", ""),
+           taxa = factor(taxa, levels = c("Collembola", "Astigmata", 
+                "Mesostigmata", "Oribatida", "Prostigmata")))
 
+p1a <- abundance %>% 
+    filter(taxa %in% c("Astigmata", "Oribatida", "Mesostigmata")) %>% 
+    group_by(seria, coast, taxa) %>%  
+    summarise(abu = sum(abu), 
+              plants.e = paste0(unique(plants.d), collapse = " / "), 
+              .groups = "drop") %>% 
+    ggplot(aes(x = seria, y = abu, fill = taxa)) + 
+    geom_col(width = 0.68) + 
+    geom_text(mapping = aes(y = 600, label = plants.e), angle = 90, color = "black") +
+    geom_text(mapping = aes(y = 900, label = coast), angle = 90, color = "black") +
+    # scale_y_reverse() +
+    scale_fill_manual(values = c("#C77CFF", "#00B8E7", "#F8766D")) + 
+    guides(fill="none") +
+    labs(y = NULL, #x = "Серия", fill = NULL) + 
+         x = NULL)+ 
+    theme(axis.text.x = element_text(angle = 90, hjust = -0.5))
+p1b <- abundance %>% 
+    group_by(seria, coast, taxa) %>% 
+    summarise(abu = sum(abu), 
+              plants.e = paste0(unique(plants.d), collapse = " / "), 
+              .groups = "drop") %>% 
+    ggplot(aes(x = seria, y = abu, fill = taxa)) + 
+    geom_col(width = 0.68) + 
+    geom_text(mapping = aes(y = 3500, label = plants.e), angle = 90, color = "black") +
+    geom_text(mapping = aes(y = 4900, label = coast), angle = 90, color = "black") +
+    scale_fill_manual(values = c("#ABA300", "#C77CFF", "#00B8E7", "#F8766D", "#00c19A"), drop = TRUE) + 
+    scale_y_reverse() +
+    scale_x_discrete(position = "top") +
+    labs(y = NULL, x = NULL, fill = NULL) + 
+    theme(axis.text.x = element_blank())
 
+p1 <- gridExtra::grid.arrange(p1a, p1b, ncol = 1, #left = "Суммарное обилие в серии, экз.")
+                              left = "Total abundance, individuals")
+ggsave("plot1.pdf", plot = p1, width = 297/25, height = 210/25)    
 
-# save.image("01.03.2023.RData")
+# Viz me 2 ----------------------------------------------------------------
 
+p2 <- div %>% 
+    select(id, coast, plants.d, Oribatida = orb_obs_qD, Mesostigmata = mst_obs_qD) %>% 
+    mutate_if(is.numeric, function(a){a[is.na(a)] <- 0; a}) %>% 
+    mutate(seria = substr(id, 1, 4), .before = 2) %>% 
+    pivot_longer(names_to = "taxa", values_to = "nsp", -c(1:4)) %>% 
+    group_by(seria, taxa) %>% 
+    summarise(
+        coast = paste0(unique(coast), collapse = " / "), 
+        plants.e = paste0(unique(plants.d), collapse = " / "), 
+        nsp_mean = mean(nsp),
+        nsp_sd   = sd(nsp), 
+        .groups = "drop") %>% 
+    mutate(ymax = nsp_mean + nsp_sd, 
+           ymin = nsp_mean - nsp_sd, 
+           ymin = case_when(ymin <= 0 ~ 0, TRUE ~ ymin)) %>% 
+    ggplot(aes(x = seria, y = nsp_mean, 
+               fill = taxa, #color = taxa,
+               ymin = ymin, ymax = ymax)) + 
+        geom_col(position = "dodge", width = 0.68) + 
+        geom_errorbar(position = "dodge", color = "black", alpha = 0.5) + 
+    geom_text(mapping = aes(y = 10, label = plants.e), angle = 90, color = "black") +
+    geom_text(mapping = aes(y = 4, label = coast), angle = 90, color = "black") +
+        scale_fill_manual(values = c("#00B8E7", "#F8766D")) + 
+        theme(axis.text.x = element_text(angle = 90)) +
+        labs(x = NULL, fill = NULL, #y = "Среднее количество видов в серии ± SD")
+             y = "Average number of species in seria ± SD")
+ggsave("plot2.pdf", p2, width = 297/25, height = 210/25)
+
+# Viz me 3 ----------------------------------------------------------------
+M2 <- PCOA2 %>% 
+    pluck("pc") %>% 
+    map_df(rbind, .id = "D") %>% 
+    separate(D, into = c("taxa", "type")) %>% 
+    left_join(div, by = "id") %>% 
+    select(taxa:plants.d) %>% 
+    mutate(
+        axis1 = case_when(taxa == "ms" & type == "bin" ~ axis1*-1, TRUE ~ axis1),
+        axis1 = case_when(taxa == "or" & type == "bin" ~ axis1*-1, TRUE ~ axis1),
+        taxa = case_when(taxa == "or" ~ "Oribatida", TRUE ~ "Mesostigmata"), 
+        type = case_when(type == "bin" ~ "Binary data (Jaccard)", 
+                         TRUE ~ "Numeric data (Bray-Curtis)")) %>% 
+    filter(!is.na(coast))
+eig <- PCOA2 %>%
+    pluck("eig") %>%
+    map(~data.frame(axis1 = .x[1], axis2 = .x[2])) %>%
+    map_df(rbind, .id = "a") %>%
+    mutate_if(is.numeric, function(a){paste0(a, " %")}) %>%
+    separate(a, into = c("taxa", "type")) %>% 
+    mutate(taxa = case_when(taxa == "or" ~ "Oribatida", TRUE ~ "Mesostigmata"), 
+           type = case_when(type == "bin" ~ "Binary data (Jaccard)", 
+                            TRUE ~ "Numeric data (Bray-Curtis)"))
+
+ggplot(M2, aes(x = axis1, y = axis2, color = coast)) + 
+    geom_point() + 
+    stat_ellipse() +
+    geom_text(aes(label = axis1, x = 0, y = -0.63), color = "black", data = eig, alpha = 0.68) +
+    geom_text(aes(label = axis2, x = -0.9, y = 0), color = "black", data = eig, alpha = 0.68, angle = 90) +
+    scale_color_manual(values = c("#00A9FF", "#0CB720", "#CD9600", "#F8766D"))+
+    facet_grid(cols = vars(type), rows = vars(taxa)) + 
+    labs(x = NULL, y = NULL, color  = NULL, #subtitle = "A. Тип берега")
+         subtitle = "A. Coast type") 
+p3b <- ggplot(M2, aes(x = axis1, y = axis2, color = plants.d)) + 
+    geom_point() + 
+    stat_ellipse() +
+    geom_text(aes(label = axis1, x = 0, y = -0.77), color = "black", data = eig, alpha = 0.68) +
+    geom_text(aes(label = axis2, x = -0.99, y = 0), color = "black", data = eig, alpha = 0.68, angle = 90) +
+    facet_grid(cols = vars(type), rows = vars(taxa)) + 
+    labs(x = NULL, y = NULL, color = NULL, #subtitle = "Б. Доминантные виды растений") + 
+         subtitle = "B. Dominant plant species") + 
+    theme(legend.text = element_text(face = "italic"))
+p3 <- gridExtra::grid.arrange(p3a, p3b, ncol = 1) #, left = "Суммарное обилие в серии, экз.")
+ggsave("plot3.pdf", p3, width = 210/25, height = 297/25)
+
+# Viz me 4 ----------------------------------------------------------------
+p4 <- rbind(ms.w, or.w) %>% 
+    pivot_longer(names_to = "id", values_to = "abu", -sp) %>% 
+    left_join(taxa, by = "sp") %>% 
+    rbind(., .) %>% 
+    mutate(type = rep(c("diff", "Oribatida & Mesostigmata"), each = 14160)) %>% 
+    filter(area2 != "unknown" | type != "Oribatida & Mesostigmata") %>% 
+    transmute(id, seria = substr(id, 1, 4), sp, 
+              area2 = factor(area2, ordered = TRUE, levels = 
+                    c("(Semi)Cosmopolitan", "Holarctic", "Palaearctic", 
+                      "European-Caucasian", "Mediterranean-Caucasian",
+                      "Caspian and/or Caucasian")), 
+              order = case_when(type == "diff" ~ order, TRUE ~ type),
+              abu, fau = case_when(abu > 0 ~ 1, TRUE ~ 0)) %>% 
+    group_by(seria, order, area2) %>% 
+    summarise_if(is.numeric, sum) %>% 
+    mutate(Community = abu/sum(abu)*100, Fauna = fau/sum(fau)*100, .keep = "unused") %>% 
+    ungroup() %>% 
+    pivot_longer(names_to = "VAR", values_to = "VAL", -1:-3) %>% 
+ggplot(aes(x = seria, y = VAL, fill = area2)) + 
+    geom_col(width = 0.68) + 
+    facet_grid(cols = vars(VAR), rows = vars(order)) + 
+    scale_fill_viridis_d() + 
+    scale_y_reverse() +
+    labs(y = "Ratio, %", x = NULL, fill = NULL) + 
+    theme(axis.text.x = element_text(angle = 90))
+# p4
+ggsave("plot4.pdf", p4, height = 210/25, width = 297/25)
